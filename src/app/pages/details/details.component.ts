296f6315d4dcab069f40499bb56e7222017537de
Antonio <adcdenis@gmail.com>
2022-08-24 17:41:36 -0300

    até aula 92

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,15 +1,35 @@
+import { PokeApiService } from './../../service/poke-api.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
-  constructor() { }
+  public pokemon:any;
+
+  constructor(
+    private activateRouter: ActivatedRoute,
+    private api: PokeApiService
+  ) {}
 
   ngOnInit(): void {
+    this.pokemons;
   }
 
+  get pokemons() {
+    const id = this.activateRouter.snapshot.params['id'];
+    const pokemon = this.api.apiGetPokemons(`${this.url}/${id}`);
+    const pokemonNome = this.api.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, pokemonNome]).subscribe((res) => {
+      this.pokemon = res;
+    });
+  }
 }
